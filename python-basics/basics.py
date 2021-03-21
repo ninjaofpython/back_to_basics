c69b43326b14fef01d0f817605bfa926106a14fb
Saa Maphinda-Lebbie <saamaphinda-lebbie13inch@Saas-MacBook-Pro.local>
2021-03-21 12:07:24 -0700

    second commit

--- python-basics/basics.py
+++ python-basics/basics.py
@@ -1,3 +1,5 @@
 days_to_minute_conversion = 60 * 24
+calculation_to_seconds = 24 * 60 * 60
 minutes_in_20_days = days_to_minute_conversion * 20
 print(f"There are {minutes_in_20_days} minutes in 20 days.")
+print(f"There are {35 * calculation_to_seconds} seconds in 35 days.")
